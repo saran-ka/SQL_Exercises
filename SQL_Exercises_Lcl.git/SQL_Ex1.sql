37920981f58982862834ea8cf7205e377e7bb42c
saran-ka <saranraj.ka@gmail.com>
2025-02-11 12:51:57 -0700

    first commit

---
+++ SQL_Exercises_Lcl.git/SQL_Ex1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE 
+SALES_DATA
+(
+PRODUCT_NAME VARCHAR2(100),
+CATEGORY VARCHAR2(100),
+SALES DECIMAL(10,7)
+)
+;
+
+ALTER TABLE SALES_DATA MODIFY(SALES NUMBER(10,6)); 
+
+select * from core.SALES_DATA; 
+
+INSERT ALL 
+INTO SALES_DATA VALUES('Product AA','Category 1', 1000.00)
+INTO SALES_DATA VALUES('Product BB','Category 2', 2000.00)
+INTO SALES_DATA VALUES('Product CC','Category 3', 3000.00)
+INTO SALES_DATA VALUES('Product DD','Category 4', 4000.00)
+SELECT 1 FROM DUAL;
+
+select * from (
+select product_name, sales, lag(sales) over (order by sales ) as next_salary
+from sales_data) a 
+where rownum<=2;
